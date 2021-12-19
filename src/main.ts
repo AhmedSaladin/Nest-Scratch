a60d1e7951742079a8c71ef141f0eea898350fec
Ahmed Saladin <as.yuri74@gmail.com>
2021-12-19 11:24:08 -0800

    Restructure repo

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from "@nestjs/core";
-import { appModule } from "./app.module";
-
-async function bootstrap() {
-  const app = await NestFactory.create(appModule);
-  await app.listen(3000);
-}
-
-bootstrap();
