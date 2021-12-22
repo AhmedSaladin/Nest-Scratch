14929d51624a46d0abcf96a45da7cd3fe2dd6233
Ahmed Saladin <as.yuri74@gmail.com>
2021-12-22 16:26:47 +0200

    Adding users routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { UserDto } from './Dtos/user.dto';
 import { UsersService } from './users.service';
 
@@ -9,5 +18,24 @@ export class UsersController {
   @Post('/signup')
   CreateUser(@Body() body: UserDto) {
     return this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  async findUser(@Param() id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  @Get()
+  async findAllUsers(@Query('email') email: string) {
+    const user = await this.usersService.find(email);
+    if (user.length === 0) throw new NotFoundException('email not found');
+    return user;
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
