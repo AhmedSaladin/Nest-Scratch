ad31677c2aa88d2719567180c001eb8c26793633
Ahmed Saladin <as.yuri74@gmail.com>
2021-12-22 07:35:49 +0200

    Update signup route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserDto } from './Dtos/user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   CreateUser(@Body() body: UserDto) {
-    console.log(body);
+    return this.usersService.create(body.email, body.password);
   }
 }
